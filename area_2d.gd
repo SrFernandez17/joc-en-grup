8b7b61b8c0b9506aed29e9053473c8156a9cf216
SrFernandez17 <fernandez.salvans.guiu@alumnes.fundaciocic.org>
2026-04-30 10:50:53 +0200

    LA RATA

---
+++ area_2d.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var velocitat := 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direccio:= Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	global_position += direccio*velocitat*delta
+
+# Called when the node enters the scene tree for the first time.
