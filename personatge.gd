b8fefdd3492604d1a03e585231a4a95e38dac595
SrFernandez17 <fernandez.salvans.guiu@alumnes.fundaciocic.org>
2026-05-14 10:21:50 +0200

    animació ratolí

--- personatge.gd
+++ personatge.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 const SPEED = 100.0
-@onready var RATA: Sprite2D = $RATA
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
 @export var escena_bala: PackedScene
 var last_dir: Vector2 = Vector2.ZERO
 
@@ -9,15 +8,16 @@ func _ready() -> void:
 	position.x = 200
 	position.y = 500
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		dispara()
 	var direccio = Input.get_vector("esquerra","dreta","amunt","avall")
 	velocity = direccio * SPEED
 	move_and_slide()
-	RATA.position = position
+	
 	if not direccio.is_zero_approx():
 		last_dir=direccio
+	anima(velocity)
 	
 
 func dispara():
@@ -25,3 +25,17 @@ func dispara():
 	%Bales.add_child(bala)
 	bala.global_position = global_position
 	bala.direction = last_dir
+
+func anima(velocity:Vector2):
+	if velocity.x > 0:
+		animated_sprite_2d.play("corre")
+		animated_sprite_2d.flip_h = false 
+	elif velocity.x < 0:
+		animated_sprite_2d.play("corre")
+		animated_sprite_2d.flip_h = true
+	
+	elif velocity.y != 0:
+		animated_sprite_2d.play("corre")
+	else:
+		animated_sprite_2d.play("aturat") 
+		
